d3f1109f06076ef67788286a4d0960b69665c7ff
web3x3 <bzwdyls@gmail.com>
2025-10-29 22:00:53 +0800

    Update Governor.sol

--- genesis-contract/contracts/Governor.sol
+++ genesis-contract/contracts/Governor.sol
@@ -63,7 +63,7 @@ contract Governor is
     mapping(address => uint256) public latestProposalIds;
 
     /*----------------- init -----------------*/
-    function initialize() external initializer onlyCoinbase onlyZeroGasPrice {
+    function initialize() external initializer {
         __Governor_init("Governor");
         __GovernorSettings_init(INIT_VOTING_DELAY, INIT_VOTING_PERIOD, INIT_PROPOSAL_THRESHOLD);
         __GovernorCompatibilityBravo_init();
